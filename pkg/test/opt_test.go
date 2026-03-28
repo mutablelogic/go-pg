1b247169d3163da838545a202f9457d7219262d9
David Thorpe <djt@mutablelogic.com>
2026-03-28 10:07:25 +0100

    Updated

--- pkg/test/opt_test.go
+++ pkg/test/opt_test.go
@@ -21,19 +21,18 @@ func Test_Opt_001(t *testing.T) {
 	require.NoError(OptEntrypoint("/bin/sh", "-c")(&o))
 	require.NoError(OptCommand([]string{"echo", "hello"})(&o))
 	require.NoError(OptEnv("A", "1")(&o))
-	require.NoError(OptPorts("3389/tcp")(&o))
+	require.NoError(OptPorts("3389/tcp", "389/tcp")(&o))
 	require.NoError(OptFile(testcontainers.ContainerFile{
 		Reader:            strings.NewReader("echo bootstrap\n"),
 		ContainerFilePath: "/bootstrap.sh",
 		FileMode:          0o755,
 	})(&o))
-	require.NoError(OptWait(wait.ForListeningPort(nat.Port("3389/tcp")))(&o))
 	require.NoError(OptWaitLog("server ready")(&o))
 
 	assert.Equal([]string{"/bin/sh", "-c"}, o.req.Entrypoint)
 	assert.Equal([]string{"echo", "hello"}, o.req.Cmd)
 	assert.Equal("1", o.req.Env["A"])
-	assert.Equal([]string{"3389/tcp"}, o.req.ExposedPorts)
+	assert.Equal([]string{"3389/tcp", "389/tcp"}, o.req.ExposedPorts)
 	if assert.Len(o.req.Files, 1) {
 		assert.Equal("/bootstrap.sh", o.req.Files[0].ContainerFilePath)
 		assert.Equal(int64(0o755), o.req.Files[0].FileMode)
@@ -42,8 +41,8 @@ func Test_Opt_001(t *testing.T) {
 	multi, ok := o.req.WaitingFor.(*wait.MultiStrategy)
 	require.True(ok)
 	assert.Len(multi.Strategies, 3)
-	assert.IsType(wait.ForExposedPort(), multi.Strategies[0])
-	assert.IsType(wait.ForListeningPort(nat.Port("3389/tcp")), multi.Strategies[1])
+	assert.IsType(wait.ForListeningPort(nat.Port("3389/tcp")), multi.Strategies[0])
+	assert.IsType(wait.ForListeningPort(nat.Port("389/tcp")), multi.Strategies[1])
 	assert.IsType(wait.ForLog("server ready"), multi.Strategies[2])
 }
 
